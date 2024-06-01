e5117dbb4cff4fe0fdf5ff9801a9fa1f720c14ae
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-06-01 05:39:59 +0300

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,3 @@
-function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(" | ");
+export default function iterateThroughObject(reportWithIterator) {
+  return reportWithIterator.join(' | ');
 }
