14d18c610b5a9933df32c4d6cea523651e8b3e00
nour-nono <nour98nour@gmail.com>
2024-06-04 04:38:51 +0000

    3-get_ids_sum.js 2nd try

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,5 +2,5 @@ export default function getStudentIdsSum(arrOfObj) {
   if (!Array.isArray(arrOfObj)) {
     return [];
   }
-  return arrOfObj.filter((acc, cur) => acc.id + cur.id);
+  return arrOfObj.reduce((acc, cur) => acc + cur.id, 0);
 }
