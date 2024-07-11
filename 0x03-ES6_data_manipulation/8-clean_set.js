f7a52d116a771b87d76a1ffa7ae280deda052db1
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-07-11 21:42:58 +0300

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,21 @@
 export default function cleanSet(set, startString) {
-  let res;
-  if (!startString || typeof startString !== 'string') {
-    return '';
+  // let res;
+  // if (!startString || typeof startString !== 'string') {
+  //   return '';
+  // }
+  // res = Array.from(set).filter((word) => typeof word === 'string' && word.startsWith(startString));
+  // res = res.map((word) => word.slice(startString.length));
+  // return res.join('-');
+  let ans = [];
+  if (!str || typeof startString !== 'string') {
+    return "";
   }
-  res = Array.from(set).filter((word) => typeof word === 'string' && word.startsWith(startString));
-  res = res.map((word) => word.slice(startString.length));
-  return res.join('-');
+  for (const s of set)
+  {
+    if (typeof s === 'string' && s.startsWith(str))
+    {
+      ans.push(s.substring(str.length));
+    }
+  }
+  return ans.join('-');
 }
