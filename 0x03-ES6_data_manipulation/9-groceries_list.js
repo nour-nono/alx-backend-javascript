068b37f6789f6acefc4cc2bed6d7cd6a5922df97
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-07-11 23:13:48 +0300

    Update 9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -6,7 +6,8 @@ export default function groceriesList() {
   // map.set('Rice', 1);
   // map.set('Banana', 5);
   // return map;
-  return new Map([["Apples", 10],
+  return new Map([
+    ["Apples", 10],
     ["Tomatoes", 10],
     ["Pasta", 1],
     ["Rice", 1],
