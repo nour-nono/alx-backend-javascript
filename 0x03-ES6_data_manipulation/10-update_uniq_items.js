57a03d7134c6563f7e503c8257203abcc6fcdbe1
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-07-11 23:14:37 +0300

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,20 +1,20 @@
 export default function updateUniqueItems(map) {
-  // if (!(map instanceof Map)) {
-  //   throw new Error('Cannot process');
-  // }
-  // map.forEach((value, key) => {
-  //   if (value === 1) {
-  //     map.set(key, 100);
-  //   }
-  // });
-  // ################################
   if (!(map instanceof Map)) {
-    throw Error("Cannot process");
+    throw new Error('Cannot process');
   }
-  for (const i of map) {
-    if (i[1] === 1) {
-      map.set(i[0], 100);
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
     }
-  }
-  return map;
+  });
+  // ################################
+  // if (!(map instanceof Map)) {
+  //   throw Error("Cannot process");
+  // }
+  // for (const i of map) {
+  //   if (i[1] === 1) {
+  //     map.set(i[0], 100);
+  //   }
+  // }
+  // return map;
 }
