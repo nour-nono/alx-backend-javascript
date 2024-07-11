342e774cbf01f3e6bc554e9486f41d4c42aa9b30
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-07-11 23:15:49 +0300

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,21 +1,22 @@
 export default function cleanSet(set, startString ) {
-  // let res;
-  // if (!startString || typeof startString !== 'string') {
-  //   return '';
-  // }
-  // res = Array.from(set).filter((word) => typeof word === 'string' && word.startsWith(startString));
-  // res = res.map((word) => word.slice(startString.length));
-  // return res.join('-');
-  let ans = [];
-  if (!startString  || typeof startString  !== 'string') {
+  let res;
+  if (!startString || typeof startString !== 'string') {
     return '';
   }
-  for (const s of set)
-  {
-    if (typeof s === 'string' && s.startsWith(startString))
-    {
-      ans.push(s.substring(startString.length));
-    }
-  }
-  return ans.join('-');
+  res = Array.from(set).filter((word) => typeof word === 'string' && word.startsWith(startString));
+  res = res.map((word) => word.slice(startString.length));
+  return res.join('-');
+  // #############################################
+  // let ans = [];
+  // if (!startString  || typeof startString  !== 'string') {
+  //   return '';
+  // }
+  // for (const s of set)
+  // {
+  //   if (typeof s === 'string' && s.startsWith(startString))
+  //   {
+  //     ans.push(s.substring(startString.length));
+  //   }
+  // }
+  // return ans.join('-');
 }
