57d122fee53948de2ad3bd0e801f61f919d28aef
nono <nour98nour@gmail.com>
2024-09-19 02:45:01 +0300

    Refactor stdin.js to properly display user input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-process.stdin.on("data", (data) => {
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.toString()}`);
   process.exit();
 });
-process.on("exit", () => {
+process.on('exit', () => {
   if (!process.stdin.isTTY) {
-    process.stdout.write("This important software is now closing\n");
+    process.stdout.write('This important software is now closing\n');
   }
 });
