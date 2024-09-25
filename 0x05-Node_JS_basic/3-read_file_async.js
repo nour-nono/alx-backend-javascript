b9d356a214f6b56846fa1afe57a48f8e21527469
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-09-25 04:23:35 +0300

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -38,19 +38,19 @@
 //     });
 //   });
 // }
-const { readFile } = require("node:fs/promises");
+const { readFile } = require('fs/promises');
 
 const countStudents = async (path) => {
-  const file = await readFile(path, "utf8");
+  const file = await readFile(path, 'utf8');
   if (!file) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
-  const lines = file.trim().split("\n");
+  const lines = file.trim().split('\n');
   lines.shift();
   let output = `Number of students: ${lines.length}\n`;
   const answer = {};
   lines.forEach((l) => {
-    const fields = l.trim().split(",");
+    const fields = l.trim().split(',');
     if (fields.length == 4 && answer[fields[3]]) {
       if (fields.length == 4) {
         answer[fields[3]].push(fields[0]);
@@ -61,7 +61,7 @@ const countStudents = async (path) => {
   });
   Object.entries(answer).forEach(([k, v]) => {
     output += `Number of students in ${k}: ${v.length}. List: ${v.join(
-      ", "
+      ', '
     )}\n`;
   });
   return output.trim();
