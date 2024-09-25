4330c30c814b5121b77aa94763eec5181c1c9fd4
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-09-26 00:26:23 +0300

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,19 +1,19 @@
 const http = require('http');
 
-// const host = '127.0.0.1';
-// const port = 1245;
+const host = '127.0.0.1';
+const port = 1245;
 
-// const app = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello Holberton School!');
-// });
-
-// app.listen(port, host, () => {});
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
-app.listen(1245);
+
+app.listen(port, host, () => {});
+// const app = http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello Holberton School!');
+// });
+// app.listen(1245);
 
 module.exports = app;
