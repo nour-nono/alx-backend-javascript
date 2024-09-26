d7dab9b418fb89fe8e0a8ae2c225c98f5d4032b6
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-09-27 02:03:54 +0300

    Update AppController.js

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,11 +1,17 @@
 /**
  * Application controller
  */
+// class AppController {
+//   static getHomepage(request, response) {
+//     response.statusCode = 200;
+//     response.send("Hello Holberton School!");
+//   }
+// }
+// export default AppController;
 class AppController {
-  static getHomepage(request, response) {
-    response.statusCode = 200;
-    response.send("Hello Holberton School!");
+  static getHomepage(req, res) {
+    res.statusCode = 200;
+    res.send('Hello Holberton School!');
   }
 }
-
 export default AppController;
