64952dbbe9e88270be9e8dcbbf2a5a5a124a2b63
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-09-27 02:05:52 +0300

    Update index.js

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -7,5 +7,5 @@ import StudentsController from '../controllers/StudentsController.js';
 const router = Router();
 router.get('/', AppController.getHomepage);
 router.get('/students', StudentsController.getAllStudents);
-router.get('/students/:major', StudentsController.getAllStudentsByMajor());
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 export default router;
