c40f73d5703037086606647989b16d4f7a979fee
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-09-27 02:00:46 +0300

    Update server.js

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -2,13 +2,9 @@
  * start express application server
  */
 import express from 'express';
-import router from './routes/index';
-
-const app = express();
+import router from './routes/index.js';
 const port = 1245;
-
+const app = express();
 app.use('/', router);
-
 app.listen(port);
-
 export default app;
